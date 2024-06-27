f5ada750bca0baedc6c277c8aa6d730f14f55368
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-06-27 12:47:28 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import { useState} from 'react';
+import AppAppBar from '../components/AppAppBar';
+import { ThemeToggleProvider } from '../styles/ThemeContext';
+import SignInSide from '../components/SignInSide';
 
-const Home = () => (
-  <div>
-    <h1>Welcome to Nexus</h1>
-  </div>
-);
+
+const Home = () => {
+    const [mode, setMode] = useState('light');
+    const toggleColorMode = () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      };
+    
+    return (
+        <ThemeToggleProvider>
+      <section>
+        <div>
+            <AppAppBar toggleColorMode={toggleColorMode} />
+        </div>
+        <div>
+            <SignInSide />
+        </div>
+      </section>
+      </ThemeToggleProvider>
+      
+    );
+  };
 
 export default Home;
