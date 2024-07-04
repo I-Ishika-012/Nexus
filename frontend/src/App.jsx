7375efa559a0924da48c2545e24007e9c0197ce0
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2024-07-04 11:47:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import Feed from './pages/Feed'
+import Network from './pages/Network'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeToggleProvider } from './styles/ThemeContext'
 
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/network" element={<Network />} />
       </Routes>
     </Router>
     </ThemeToggleProvider>
